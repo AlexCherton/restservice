98908b6822f40aa65941996e07e234a024540da2
alex <alex_general2002@mail.ru>
2025-07-01 13:38:14 +0300

    First

--- src/main/java/com/home/restservice/entity/Person.java
+++ src/main/java/com/home/restservice/entity/Person.java
@@ -1,15 +1,31 @@
 package com.home.restservice.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 
 @Data
 @Getter
 @Setter
+@Entity
+@Table(name = "People")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
     private String name;
     private int age;
+
+    public Person(String name, int age, int snils) {
+        this.name = name;
+        this.age = age;
+        this.snils = snils;
+    }
+
+
+
     private int snils;
 
 }
---
+++ src/main/java/com/home/restservice/repositories/PersonRepo.java
@@ -0,0 +1,9 @@
+package com.home.restservice.repositories;
+
+import com.home.restservice.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepo extends JpaRepository<Person, Integer> {
+}
